e55bd110c7e4371fd1f4e828b41f4ea7437f87f6
DELL <shivanikumari28@gmail.com>
2024-03-30 23:21:25 +0530

    folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react'
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return (
+    <main>
+     {children}
+    </main>
+  )
+}
+
+export default RootLayout 
+
