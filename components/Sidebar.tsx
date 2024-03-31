0128d195785f680caa623d813eaada5ed79c809c
DELL <shivanikumari28@gmail.com>
2024-03-31 20:45:44 +0530

    public folder add

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+'use client'
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const Sidebar = () => {
+    const pathname = usePathname();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link)=>{
+            const isActive = pathname === link.route ||pathname.startsWith(link.route);
+            return (
+                <Link
+                  href={link.route}
+                  key={link.label}
+                  className={cn(
+                    'flex gap-4 items-center p-4 rounded-lg justify-start',
+                    {
+                      'bg-blue-1': isActive,
+                    }
+                  )}
+                >
+                  {link.label}
+                </Link>
+              );
+        })}
+        </div>
+    </section>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
