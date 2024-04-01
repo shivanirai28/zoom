a1f72404b6cec62e236c6594d95ad11029574c03
DELL <shivanikumari28@gmail.com>
2024-04-01 18:23:54 +0530

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          YOOM
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+      <MobileNav />
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
