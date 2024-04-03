39b85089d783600abf9eb28515e2567035fbe884
DELL <shivanikumari28@gmail.com>
2024-04-03 20:29:13 +0530

    Home page layout complete

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,25 +1,35 @@
-import Image from "next/image";
-import React from "react";
+'use client';
 
-const HomeCard = () => {
+import Image from 'next/image';
+
+import { cn } from '@/lib/utils';
+
+interface HomeCardProps {
+  className?: string;
+  img: string;
+  title: string;
+  description: string;
+  handleClick?: () => void;
+}
+
+const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
   return (
-    <div
-      className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
-      onClick={() => {}}
+    <section
+      className={cn(
+        'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
+        className
+      )}
+      onClick={handleClick}
     >
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
-        <Image
-          src="/icons/add-meeting.svg"
-          alt="meeting"
-          width={27}
-          height={27}
-        />
+        <Image src={img} alt="meeting" width={27} height={27} />
       </div>
+      
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">New Meeting</h1>
-        <p className="text-lg font-normal">Start an instant meeting</p>
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-lg font-normal">{description}</p>
       </div>
-    </div>
+    </section>
   );
 };
 
