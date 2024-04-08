278ab536365d472da811e02216b12088b9c4238c
DELL <shivanikumari28@gmail.com>
2024-04-09 00:24:00 +0530

    meeting acll setup🎥

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+'use client';
+import Loader from '@/components/Loader';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: {id} }: { params: { id: string } }) => {
+  const { isLoaded, user } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  
+
   return (
-    <div>
-      Meeting Room: #{params.id}
-    </div>
+    <main className="h-screen w-full">
+      <StreamCall call ={call}>
+        <StreamTheme>
+
+        {!isSetupComplete ? (
+          <MeetingSetup/>
+        ) : (
+          <MeetingRoom/>
+        )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
