e167e6a37887b78a227c5a8a7b16615b8b7be399
DELL <shivanikumari28@gmail.com>
2024-04-11 22:44:01 +0530

    recording room

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import CallList from '@/components/CallList';
 
-const Recording = () => {
+const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-  <h1 className="text-3xl font-bold">Recording</h1>
-</section>
-  )
-}
+      <h1 className="text-3xl font-bold">Recordings</h1>
 
-export default Recording
+      <CallList type="recordings" />
+    </section>
+  );
+};
+
+export default PreviousPage;
